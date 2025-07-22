5ad29298594b699d7b9299dc23e531a50e63e8c1
Kiitoksia <matt.ward92@gmail.com>
2025-07-22 16:59:05 +0100

    Start of UltimeciaManipPage

--- FF8Utilities.MAUI/UltimeciaManipPage.xaml.cs
+++ FF8Utilities.MAUI/UltimeciaManipPage.xaml.cs
@@ -5,12 +5,51 @@ namespace FF8Utilities.MAUI;
 
 public partial class UltimeciaManipPage : ContentPage
 {
-	public UltimeciaManipPage()
+
+    public UltimeciaManipPage()
 	{
 		InitializeComponent();
 
         Directions = new BindingList<Direction>();
+		Directions.ListChanged += async (s, e) =>
+		{
+			if (Directions.Count >= 12)
+			{
+                await Calculate();
+            }
+		};
+
+
         SubmitDirectionCommand = new Command<Direction>(SubmitDirection);
+
+    }	
+
+	public BindingList<PartyFormation> Formations { get; set; }
+	
+
+	private Common.Platform Platform
+	{
+		get => Enum.Parse<Common.Platform>(Preferences.Get(nameof(Platform), Common.Platform.PS2.ToString()));
+		set => Preferences.Set(nameof(Platform), value.ToString());
+    }
+
+	private UltimeciaManipLanguage Language
+	{
+		get => Enum.Parse<UltimeciaManipLanguage>(Preferences.Get(nameof(Language), UltimeciaManipLanguage.English.ToString()));
+		set => Preferences.Set(nameof(Language), value.ToString());
+    }
+
+	private async Task Calculate()
+	{
+        UltimeciaManipLanguage[] languages = Manipulation.GetSupportedLanguages(Platform);
+		if (!languages.Contains(Language))
+		{
+			await DisplayAlert("Unsupported Language", $"The selected language ({Language}) is not supported for the current platform ({Platform}).", "OK");
+			return;
+        }
+
+
+        PartyFormation[] formations = Manipulation.GetUltimeciaFormations(Directions.ToArray(), Platform, Language);
 
     }
 
@@ -19,7 +58,7 @@ public partial class UltimeciaManipPage : ContentPage
         Directions.Add(direction);
 	}
 
-	public BindingList<Direction> Directions { get; set; }
+	public BindingList<Direction> Directions { get; }
 
 	public Command SubmitDirectionCommand { get; }
 
