37e29f3e0421d4027352bd71461bf7be4bce27d0
Kiitoksia <matt.ward92@gmail.com>
2026-05-14 16:23:00 +0100

    Card control rendering on Web

--- FF8Utilities.Web/Models/CardManipulationModel.cs
+++ FF8Utilities.Web/Models/CardManipulationModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
-using Blazored.LocalStorage;
 using CardManipulation;
 using FF8Utilities.Common;
 using FF8Utilities.Web.Services;
@@ -10,45 +10,62 @@ namespace FF8Utilities.Web.Models
     public class CardManipulationModel : BaseCardManipulationModel
     {
         private ISettingsService _settings;
+        private CancellationTokenSource? _timerCts;
 
-
-        public CardManipulationModel(CardManip manip, uint state, string player, int? delayFrames, int? rngModifier, ISettingsService settings) : base(manip, state, player, delayFrames, rngModifier)
+        public CardManipulationModel(CardManip manip, uint state, string player, int? delayFrames, int? rngModifier, ISettingsService settings)
+            : base(manip, state, player, delayFrames, rngModifier)
         {
             _settings = settings;
         }
 
         public override int CountdownTimer => _settings?.CountdownTimer ?? 3;
-
         public override int BeepInterval => _settings?.BeepInterval ?? 400;
-
         public override int BeepCount => _settings?.BeepCount ?? 4;
-
         public override int BeepOffsetFrames => _settings?.BeepOffsetFrames ?? 0;
-
         public override Platform Platform => _settings?.Platform ?? Platform.PC;
+
+        public event Action Redraw;
 
         public override void TimerStarted()
         {
-            
+            _timerCts = new CancellationTokenSource();
+            _ = RunRenderLoopAsync(_timerCts.Token);
         }
 
         public override void TimerStopped()
         {
+            _timerCts?.Cancel();
+            _timerCts = null;
+        }
+
+        private async Task RunRenderLoopAsync(CancellationToken ct)
+        {
+            using(var timer = new PeriodicTimer(TimeSpan.FromMilliseconds(16.67)))
+            {
+                try
+                {
+                    while (await timer.WaitForNextTickAsync(ct))
+                    {
+                        Refresh();
+                        Redraw?.Invoke();
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+
+                }
+            }
             
         }
 
         protected override void PauseBeeps()
         {
-           
-        }
 
+        }
         protected override void PlayBeeps(BeepSchedule schedule)
         {
-            
+
         }
-
-
-
 
         public async Task Initialise()
         {
