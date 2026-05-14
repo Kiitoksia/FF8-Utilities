197ca4f89980800196af25375a6fb6bd529f225f
Kiitoksia <matt.ward92@gmail.com>
2026-05-15 00:44:44 +0100

    Layout tweaks

---
+++ FF8Utilities.Web/WebHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace FF8Utilities.Web
+{
+    public static class WebHelper
+    {
+        public static string ToCss(this Color c) => $"rgba({c.R},{c.G},{c.B},{c.A / 255f:F3})";
+
+    }
+}
