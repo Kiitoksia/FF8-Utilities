361a2ccec63e3d80e56591ba30a4fa49b635d4db
Kiitoksia <matt.ward92@gmail.com>
2026-05-16 00:35:19 +0100

    Changed from URI parsing to service

--- FF8Utilities.Web/Program.cs
+++ FF8Utilities.Web/Program.cs
@@ -13,6 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ISettingsService, SettingsService>();
+builder.Services.AddSingleton<CardTrackerStateService>();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazorBootstrap();
 
---
+++ FF8Utilities.Web/Services/CardTrackerStateService.cs
@@ -0,0 +1,9 @@
+﻿using FF8Utilities.Common;
+
+namespace FF8Utilities.Web.Services
+{
+    public class CardTrackerStateService
+    {
+        public EarlyQuistisPattern SelectedEarlyQuistisPattern { get; set; }
+    }
+}
