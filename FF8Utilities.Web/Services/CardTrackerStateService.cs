2487bd20c248e4c35b7e00ff85652a2121ff71d3
Kiitoksia <matt.ward92@gmail.com>
2026-06-15 13:37:08 +0100

    Card manipulation pushes to new page

--- FF8Utilities.Web/Services/CardTrackerStateService.cs
+++ FF8Utilities.Web/Services/CardTrackerStateService.cs
@@ -1,9 +1,28 @@
 ﻿using FF8Utilities.Common;
+using FF8Utilities.Common.Cards;
+using FF8Utilities.Web.Models;
 
 namespace FF8Utilities.Web.Services
 {
     public class CardTrackerStateService
     {
         public EarlyQuistisPattern SelectedEarlyQuistisPattern { get; set; }
+
+        public LateQuistisPattern SelectedLateQuistisPattern { get; set; }
+
+        public uint? LateQuistisResult { get; set; }
+
+        public CardManipulationModel ManipModel { get; set; }
+
+        /// <summary>
+        /// Clears all current card values
+        /// </summary>
+        public void Reset()
+        {
+            SelectedEarlyQuistisPattern = null;
+            SelectedLateQuistisPattern = null;
+            LateQuistisResult = null;
+            ManipModel = null;
+        }
     }
 }
